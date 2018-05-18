218a64770640f65dd2336f3d19e0f26bc8aebe6c
meteorodev <darwin11rv@gmail.com>
2018-05-17 23:34:16 -0500

    metodo para proceso de datos diarios

--- util/loadNetcdf.py
+++ util/loadNetcdf.py
@@ -19,7 +19,42 @@ class LoadNetcdf():
         print(ncMeta.variables['time'])
         ncMeta.close()
 
-    def creaMensuales(self, ncvar):
+    def leeDiarios(self,ncvar,año):
+        datanc=nc(self.ncfile)
+        dataset = datanc.variables[ncvar][:]
+        lon = datanc.variables['lon'][:]
+        lat = datanc.variables['lat'][:]
+        head = ["lat", "lon", "ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
+        dias = [31,28,31,30,31,30,31,31,30,31,30,31]
+        if (año % 4 == 0 and año % 100 != 0 or año % 400 == 0):
+            dias[1]=29
+        rows = []
+        #print(np.shape(dataset))
+        for i in range(0,len(lat)):#recorre las latitudes
+            for j in range(0,len(lon)):#recorre las longitudes
+                row = []
+                mes = []
+                row.extend([lat[i]])
+                row.extend([lon[j]])
+                di=0
+                df=0
+                for m in range(0,12):
+                    df=di+dias[m]
+                    mes.extend([np.mean(dataset[di:df,i,j])])
+                    #print("dia inicio => ", di, " sumado ", dias[m], "dis es  : dia fin =>", df, " el promedio", mes)
+                    di=df
+                row.extend(mes)
+                rows.append(row)
+        datadf = pd.DataFrame(rows, columns=head)
+        #print(datadf)
+
+        datanc.close()
+
+        return datadf
+
+
+
+    def leeMensuales(self, ncvar):
         """lee el archivo nc y crea archivos de texto mensuales """
         datanc = nc(self.ncfile)
         dataset=datanc.variables[ncvar][:]
@@ -51,7 +86,7 @@ class LoadNetcdf():
         lat = datanc.variables['lat'][:]
         head = ["lat", "lon","EFM", "AMJ", "JAS", "OND"]
         rows = []
-        #print(np.shape(dataset))
+        print(np.shape(dataset))
         for i in range(0, len(lat)):  # recorre las latitudes
             for j in range(0, len(lon)):  # recorre las longitudes
                 #row = [lat[i],lon[j],sum(dataset[0:3, i, j])]
@@ -77,16 +112,38 @@ class LoadNetcdf():
 
         return datadf
 
+    def mes2Trim(self,mensual,funcion):
+        head = ["lat", "lon", "EFM", "AMJ", "JAS", "OND"]
+        rows = []
+        temData = mensual.iloc[:,[0,1]].copy()
+
+        if funcion==1:#para la suma
+            temData['EFM'] = mensual['ene'] + mensual['feb'] + mensual['mar']
+            temData['AMJ'] = mensual['abr'] + mensual['may'] + mensual['jun']
+            temData['JAS'] = mensual['jul'] + mensual['ago'] + mensual['sep']
+            temData['OND'] = mensual['oct'] + mensual['nov'] + mensual['dic']
+        else: # para el promedio
+            temData['EFM'] = mensual.iloc[:, 2:5].mean(axis=1)
+            temData['AMJ'] = mensual.iloc[:, 5:8].mean(axis=1)
+            temData['JAS'] = mensual.iloc[:, 8:11].mean(axis=1)
+            temData['OND'] = mensual.iloc[:, 11:14].mean(axis=1)
+
+        return temData
 
 
+
+
+
+""" 
 ####test
-"""
-filenc="/media/darwin/Darwin/Mensuales/Prec/RCP45/pr_day_Ecuador_Ensamble_rcp45_2011.nc"
+
+filenc="/media/darwin/Darwin/Diarios/TMax/RCP45/tasmax_day_Ecuador_CSIRO-Mk3-6-0_rcp45_2012.nc"
 
 tcn=LoadNetcdf(filenc)
 #tcn.getInfo()
 #tcn.creaMensuales('precip')
-tcn.creaTrim('precip').to_csv("~/Escritorio/trimestre.csv",sep=";",index=False)
-
-
+#tcn.creaTrim('precip').to_csv("~/Escritorio/trimestre.csv",sep=";",index=False)
+data=tcn.leeDiarios('tmax',2012)
+datTrim=tcn.mes2Trim(data,2)
+print(datTrim)
 """
--- view/process.py
+++ view/process.py
@@ -38,12 +38,37 @@ class Process():
         ##
         return dattemp
 
-
+    def procesPeriodDia(self,añoi,añof,var='precip',funcion=1):
+        """Une los archivos generados por cada año en un solo dataframe" a partir de los archivos diarios"""
+        cab=["lat", "lon"]
+        for i in range(añoi,añof+1):
+            cab.extend(['EFM'+str(i),'AMJ'+str(i),'JAS'+str(i),'OND'+str(i)])
+            ncfile=self.rootPath+str(i)+".nc"
+            nc=lnc.LoadNetcdf(ncfile=ncfile)
+            print("procesando archivo ",ncfile)
+            if(i==añoi):
+                data = nc.leeDiarios(var, i)
+                dattemp = nc.mes2Trim(data, funcion)
+            else:
+                data = nc.leeDiarios(var, i)
+                datTrim = nc.mes2Trim(data, funcion)
+                dattemp = pd.concat([dattemp,datTrim.loc[:,["EFM", "AMJ", "JAS", "OND"]]],axis=1,join='inner')
+        dattemp.columns = cab
+        #print(np.shape(dattemp))
+        #print(len(cab))
+        """"Genera promedios por trimestre y agrega las columnas"""
+        dattemp["EFM_TOT"] = dattemp.loc[:, dattemp.columns.map(lambda x: x.startswith("EFM"))].mean(axis=1)
+        dattemp["AMJ_TOT"] = dattemp.loc[:, dattemp.columns.map(lambda x: x.startswith("AMJ"))].mean(axis=1)
+        dattemp["JAS_TOT"] = dattemp.loc[:, dattemp.columns.map(lambda x: x.startswith("JAS"))].mean(axis=1)
+        dattemp["SON_TOT"] = dattemp.loc[:, dattemp.columns.map(lambda x: x.startswith("OND"))].mean(axis=1)
+        ##
+        return dattemp
 
 
 ######seccion de ejecucion
 #/media/drosero/Darwin/Mensuales/Prec/RCP45/pr_day_Galapagos_IPSL-CM5A-MR_rcp45_2064.nc
-guar="/media/drosero/Datos/DATOSTCN/"
+#guar="/media/drosero/Datos/DATOSTCN/"
+guar="/home/darwin/Documentos/Datos/Chrips/DATOSTCN/"
 """
 ####Datos de precipitacion con rcp 45
 preci="/media/drosero/Darwin/Mensuales/Prec/RCP45/pr_day_Ecuador_IPSL-CM5A-MR_rcp45_"
@@ -70,89 +95,89 @@ data.to_csv(guar+"rr_2031-2050rcp85.csv", sep=";", index=False, encoding="utf-8"
 ####periodo 2051-2070
 data=pr.procesPeriod(2051,2070)
 data.to_csv(guar+"rr_2051-2070rcp85.csv", sep=";", index=False, encoding="utf-8")
-
+"""
 #################################################################################
 #################################################################################
 ####Datos de temperatura maxima con rcp 45
-preci="/media/drosero/Darwin/Mensuales/TMax/RCP45/tasmax_day_Ecuador_IPSL-CM5A-MR_rcp45_"
+preci="/media/darwin/Darwin/Diarios/TMax/RCP45/tasmax_day_Ecuador_Ensamble_rcp45_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2011-2030rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2031-2050rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2051-2070rcp45.csv", sep=";", index=False, encoding="utf-8")
 
 ####Datos de temperatura maxima con rcp 85
 #/media/drosero/Darwin/Mensuales/TMax/RCP85/tasmax_day_Ecuador_IPSL-CM5A-MR_rcp85_2022.nc
 #preci="/media/drosero/Darwin/Mensuales/TMax/RCP85/tasmax_day_Ecuador_IPSL-CM5A-MR_rcp85_"
-preci="/media/drosero/Darwin/Mensuales/TMax/RCP85/tasmax_day_Ecuador_IPSL-CM5A-MR_rcp85_"
+preci="/media/darwin/Darwin/Diarios/TMax/RCP85/tasmax_day_Ecuador_Ensamble_rcp85_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2011-2030rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2031-2050rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='tmax',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='tmax',funcion=2)
 data.to_csv(guar+"tmax_2051-2070rcp85.csv", sep=";", index=False, encoding="utf-8")
 
 
 #################################################################################
 #################################################################################
 ####Datos de temperatura minima con rcp 45
-preci="/media/drosero/Darwin/Mensuales/TMin/RCP45/tasmin_day_Ecuador_IPSL-CM5A-MR_rcp45_"
+preci="/media/darwin/Darwin/Diarios/TMin/RCP45/tasmin_day_Ecuador_Ensamble_rcp45_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2011-2030rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2031-2050rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2051-2070rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####Datos de temperatura minima con rcp 85
-preci="/media/drosero/Darwin/Mensuales/TMin/RCP85/tasmin_day_Ecuador_IPSL-CM5A-MR_rcp85_"
+preci="/media/darwin/Darwin/Diarios/TMin/RCP85/tasmin_day_Ecuador_Ensamble_rcp85_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2011-2030rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2031-2050rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='tmin',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='tmin',funcion=2)
 data.to_csv(guar+"tmin_2051-2070rcp85.csv", sep=";", index=False, encoding="utf-8")
 
-"""
+
 #################################################################################
 #################################################################################
 ####Datos de temperatura media con rcp 45
-preci="/media/drosero/Darwin/Mensuales/TMed/RCP45/tas_day_Ecuador_IPSL-CM5A-MR_rcp45_"
+preci="/media/darwin/Darwin/Diarios/TMed/RCP45/tas_day_Ecuador_Ensamble_rcp45_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='temp',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2011-2030rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='temp',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2031-2050rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='temp',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2051-2070rcp45.csv", sep=";", index=False, encoding="utf-8")
 ####Datos de temperatura minima con rcp 85
-preci="/media/drosero/Darwin/Mensuales/TMed/RCP85/tas_day_Ecuador_IPSL-CM5A-MR_rcp85_"
+preci="/media/darwin/Darwin/Diarios/TMed/RCP85/tas_day_Ecuador_Ensamble_rcp85_"
 ####periodo 2011-2030
 pr=Process(preci)
-data=pr.procesPeriod(2011,2030,var='temp',funcion=2)
+data=pr.procesPeriodDia(2011,2030,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2011-2030rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2031-2050
-data=pr.procesPeriod(2031,2050,var='temp',funcion=2)
+data=pr.procesPeriodDia(2031,2050,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2031-2050rcp85.csv", sep=";", index=False, encoding="utf-8")
 ####periodo 2051-2070
-data=pr.procesPeriod(2051,2070,var='temp',funcion=2)
+data=pr.procesPeriodDia(2051,2070,var='temp',funcion=2)
 data.to_csv(guar+"tmed_2051-2070rcp85.csv", sep=";", index=False, encoding="utf-8")
